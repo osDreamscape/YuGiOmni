ec581cac1df6e0e9e5bf71750dfeee788e4e20ba
Matt B <73551894+osDreamscape@users.noreply.github.com>
2023-08-17 16:36:34 -0700

    Update app.js
    Documentation and removed unused code

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// a REST service that accepts parameters from a webpage and returns JSON data
-// for Yu-Gi-Oh cards from a Google Cloud hosted database
+// A REST API service that accepts parameters from a webpage and returns JSON data
+// for Yu-Gi-Oh cards from a Google Cloud hosted database.
 // author: Matt Bauchspies - mbauchspies@protonmail.com / mbauch72@uw.edu
 
 // import required module
@@ -7,16 +7,18 @@ const  express = require('express');
 const mysql = require('mysql');
 const app = express();
 
-// required for bypassing Access-Control-Allow-Origin
-// may require "npm i cors express" to be ran if the error persists
-// if you have a different origin, feel free to edit this value to your origin
+// Required for bypassing Access-Control-Allow-Origin
+// If you have errors related to this, change the origin to your IP address
+// This should only be the case if you are hosting the front-end on a server on the same system
+// as the backend. 
+// If that is your use case, try opening index.html in your browser instead.
 const cors = require('cors');
 app.use(cors({
     origin: 'null'
 }));
 
 
-// Establishes address and credentials of the database used in the API
+// Establishes address and credentials of the database used in the API.
 const con = mysql.createConnection({
     host: '34.145.63.173',
     port: 3306,
@@ -24,23 +26,22 @@ const con = mysql.createConnection({
     database: 'yugiomni'
 });
 
-// Establishes connection to the cloud database
+// Establishes connection to the cloud database.
 con.connect(function(err) {
     if (err) 
         return console.err(err);
-    console.log('connected')
+    console.log('Established connection to YuGiOmni Cloud Database.')
 });
 
 
 
-// define a route using a callback function that will be invoked
-// when the user makes a HTTP request to the root of the folder (URL)
+// Send a response if the main page is pinged to acknowledge that the API is online.
 app.get('/', function (req, res) {
     res.status(200);
     res.send("This REST service is currently active.");
 });
 
-// performs a single SQL SELECT query on the YuGiOmni database
+// Executes a single arbitrary SQL SELECT query on the YuGiOmni database.
 app.get('/select/:attribute/:from/:where', function(req, res) {
     let attribute = req.params.attribute;
     let from = req.params.from;
@@ -51,11 +52,11 @@ app.get('/select/:attribute/:from/:where', function(req, res) {
     con.query(query, function (err, result, fields) {
         if (err) res.status(400);
         res.json(result);
-        console.log(result)
+        // console.log(result)
       });
 });
 
-// performs a single SQL SELECT query on the YuGiOmni database
+// Executes a single SQL SELECT query for a specific monster type.
 app.get('/monster_select/:type/', function(req, res) {
     let type = req.params.type;
     console.log("Type requested:" + type);
@@ -65,12 +66,12 @@ app.get('/monster_select/:type/', function(req, res) {
     con.query(query, function (err, result, fields) {
         if (err) res.status(400);
         res.json(result);
-        console.log(result)
+        // console.log(result)
       });
 });
 
 
-// performs a single SQL SELECT query on the YuGiOmni database, after joining the Card IDs and a specified relation
+// Executes a single SQL SELECT query on the YuGiOmni database, after joining the Card IDs and a specified relation.
 app.get('/special_select/:attribute/:from/:name', function(req, res) {
     let attribute = req.params.attribute;
     let from = req.params.from;
@@ -86,7 +87,7 @@ app.get('/special_select/:attribute/:from/:name', function(req, res) {
       });
 });
 
-// performs a single SQL SELECT query on the YuGiOmni database, after joining the Card IDs and a specified relation
+// Executes a single SQL SELECT query on the YuGiOmni database, after joining the Card IDs and a specified relation.
 app.get('/example_query/atk_def', function(req, res) {
     let query = "SELECT card_name, attack, defense FROM card_names NATURAL JOIN monster_cards WHERE attack = defense";
     con.query(query, function (err, result, fields) {
@@ -97,158 +98,8 @@ app.get('/example_query/atk_def', function(req, res) {
 });
 
 
-// calculates a melee accuracy value
-app.get('/meleeaccuracy/:level/:boost/:bonus/:prayer/:set/:style', function(req, res) {
-    const level = parseInt(req.params.level);
-    let boost = req.params.boost;
-    const bonus = parseInt(req.params.bonus);
-    const prayer = parseInt(req.params.prayer);
-    const set = req.params.set;
-    const style = parseInt(req.params.style);
-
-    const setbonuses = ["none", "void", "voidsalve", "voidsalvee", "slayer", "salve", "salvee"];
-    const boosts = ["none", "attackpotion", "superattackpotion", "overload"];
-
-    if (isNaN(level) || !(boosts.includes(boost)) || isNaN(bonus) || isNaN(prayer) || !(setbonuses.includes(set)) || isNaN(style))
-    {
-        res.status(400);
-        console.log("???")
-        res.json({error: "Bad request, a value provided was invalid."});
-        return;
-    }
-
-    // determines the boost amount from the potion used
-    switch(boost) {
-        case 'none' :
-            boost = 0;
-            break;
-        case 'attackpotion' :
-            boost = Math.floor(level * 0.1) + 3;
-            break;
-        case 'superattackpotion' :
-            boost = Math.floor(level * 0.15) + 5;
-            break;
-        case 'overload' :
-            boost = Math.floor(level * 0.16) + 6;
-            break;
-    }
-
-    let effectiveAtk = Math.floor((level + boost) * (prayer/100 + 1) + 8 + style);
-
-    if(set.includes("void")) {
-        effectiveAtk = Math.floor(effectiveAtk * 1.1);
-    }
-
-    let accuracy = Math.floor(effectiveAtk * (bonus + 64));
-
-    if (set.includes("salvee")) {
-        accuracy = Math.floor(accuracy * 1.2);
-    } else if (set.includes("salve") || set.includes("slayer")) {
-        accuracy = Math.floor(accuracy * (7/6))
-    } 
-
-        // Returns a JSON response with the values the user provided as well as the calculated accuracy.
-        res.json({ "attack_level" : level, "visible_boost" : boost, "attack_bonus" : bonus, "prayer" : prayer + "%", "set_bonus" : set, "style" : "+" + style, "effective_level" : effectiveAtk, "accuracy_roll" : accuracy});
-})
-
-// calculates a melee defence value
-app.get('/meleedefence/:level/:boost/:bonus/:prayer/:style/:monster', function(req, res) {
-    const level = parseInt(req.params.level);
-    let boost = req.params.boost;
-    const bonus = parseInt(req.params.bonus);
-    const prayer = parseInt(req.params.prayer);
-    const style = parseInt(req.params.style);
-    const monster = parseInt(req.params.monster);
-
-    const boosts = ["none", "defencepotion", "superdefencepotion", "saradominbrew", "overload"];
-
-    if (isNaN(level) || !(boosts.includes(boost)) || isNaN(bonus) || isNaN(prayer) || isNaN(style) || isNaN(monster))
-    {
-        res.status(400);
-        console.log("???")
-        res.json({error: "Bad request, a value provided was invalid."});
-        return;
-    }
-
-        // determines the boost amount from the potion used
-        switch(boost) {
-            case 'none' :
-                boost = 0;
-                break;
-            case 'defencepotion' :
-                boost = Math.floor(level * 0.1) + 3;
-                break;
-            case 'superdefencepotion' :
-                boost = Math.floor(level * 0.15) + 5;
-                break;
-            case 'saradomin brew' :
-                boost = Math.floor(level*0.2) + 2;
-                break;
-
-            case 'overload' :
-                boost = Math.floor(level * 0.16) + 6;
-                break;
-        }
-
-    let effectiveDef;
-
-    if(monster === 0) {
-        effectiveDef = Math.floor((level + boost) * (prayer/100 + 1)) + style + 8;
-    } else {
-        effectiveDef = level + 9;
-    }
-
-    let defence = Math.floor(effectiveDef * (bonus + 64))
-
-        // Returns a JSON response with the values the user provided as well as the calculated accuracy.
-        res.json({ "defence_level" : level, "visible_boost" : boost, "defence_bonus" : bonus, "prayer" : prayer + "%", "style" : "+" + style, "effective_level" : effectiveDef, "defence_roll" : defence});
-})
-
-// calculates a melee hit chance given an attack roll and a defence roll
-app.get('/meleehitchance/:accuracy/:defence', function(req, res) {
-    const accuracy = parseInt(req.params.accuracy);
-    const defence = parseInt(req.params.defence);
-    if (isNaN(accuracy) || isNaN(defence))
-    {
-        res.status(400);
-        res.json({error: "Bad request, a value provided was invalid."});
-        return;
-    }
-
-    let hitchance;
-
-    if(accuracy > defence) {
-        hitchance = 1 - ((defence + 2) / (2 * (accuracy + 1)));
-    } else {
-        hitchance = ((accuracy)/(2*(defence + 1)));
-    }
-    res.json({ "accuracy" : accuracy, "defence" : defence, "hitchance" : hitchance})
-})
-
-app.get('/meleedps/:hitchance/:maxhit/:attackspeed', function(req, res) {
-    const hitChance = parseFloat(req.params.hitchance);
-    const maxHit = parseInt(req.params.maxhit);
-    let attackSpeed = parseInt(req.params.attackspeed);
-
-    if (isNaN(hitChance) || isNaN(maxHit) || isNaN(attackSpeed))
-    {
-        res.status(400);
-        res.json({error: "Bad request, a value provided was invalid."});
-        return;
-    }
-
-    // converts attack speed in game ticks to attack speed in seconds
-    attackSpeed = attackSpeed * 0.6;
-
-    let DPS = hitChance * (maxHit/2) / attackSpeed;
-
-    res.json({"DPS" : DPS});
-
-})
-
-
 
 // enable a port to listen to incoming HTTP requests
 app.listen(3000, function() {
-    console.log("Listening on port 3000...");
+    console.log("YuGiOmni API listening on port 3000...");
 });
